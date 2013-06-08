scale-proactive/scale-proactive

commit 47fbf8fc72e09c5846b645a2c934de1fe638212c

Extracted default multiactive serving policy into its dedicated class file

src/Core/org/objectweb/proactive/multiactivity/execution/RequestExecutor.java
@@ -61,8 +61,8 @@ import org.objectweb.proactive.core.config.CentralPAPropertyRepository;
 import org.objectweb.proactive.core.util.log.Loggers;
 import org.objectweb.proactive.core.util.log.ProActiveLogger;
 import org.objectweb.proactive.multiactivity.ServingController;
-import org.objectweb.proactive.multiactivity.ServingPolicy;
 import org.objectweb.proactive.multiactivity.compatibility.CompatibilityTracker;
+import org.objectweb.proactive.multiactivity.policy.ServingPolicy;
 import org.objectweb.proactive.multiactivity.priority.PriorityConstraint;
 import org.objectweb.proactive.multiactivity.priority.PriorityGroup;
 import org.objectweb.proactive.multiactivity.priority.PriorityManager;
@@ -142,11 +142,6 @@ public class RequestExecutor implements FutureWaiter, ServingController {
      * 'resume' the waiting when the second request finishes execution
      */
     private ConcurrentHashMap<RunnableRequest, RunnableRequest> hostMap;
-
-    private HashSet<Request> invalid = new HashSet<Request>();
-
-    private HashMap<Request, Set<Request>> invalidates =
-            new HashMap<Request, Set<Request>>();
 
     /*
      * This counter allows to warn the multiactivity framework that a thread has 
@@ -274,25 +269,6 @@ public class RequestExecutor implements FutureWaiter, ServingController {
     /**
      * This is the heart of the executor. It is an internal scheduling thread
      * that coordinates wake-ups, and waits and future value arrivals. Before
-     * doing that it also starts a thread for the queue handler.
-     */
-    public void execute() {
-
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                requestQueueHandler();
-
-            }
-        }, "Request listener for " + body).start();
-
-        internalExecute();
-    }
-
-    /**
-     * This is the heart of the executor. It is an internal scheduling thread
-     * that coordinates wake-ups, and waits and future value arrivals. Before
      * doing that it also starts a thread for the queue handler that uses a
      * custom policy for scheduling.
      * 
@@ -314,64 +290,6 @@ public class RequestExecutor implements FutureWaiter, ServingController {
     }
 
     /**
-     * Method which schedules requests with the default policy.
-     */
-    private void requestQueueHandler() {
-
-        // register thread, so we can look up the Body if needed
-        LocalBodyStore.getInstance().pushContext(new Context(body, null));
-
-        synchronized (requestQueue) {
-            while (body.isActive()) {
-
-                // get compatible ones from the queue
-                List<Request> rc;
-                rc = runDefaultPolicy();
-
-                if (rc.size() >= 0) {
-                    synchronized (this) {
-                        // add them to the ready set
-                        for (int i = 0; i < rc.size(); i++) {
-                            RunnableRequest runnableRequest =
-                                    wrapRequest(rc.get(i));
-                            priorityManager.register(runnableRequest);
-                        }
-
-                        // if anything can be done, let the other thread know
-                        if (countActive() < THREAD_LIMIT) {
-                            this.notify();
-                        } else {
-                            // same for boosted methods
-                            Iterator<List<PriorityConstraint>> it =
-                                    this.priorityManager.getPriorityConstraints()
-                                            .values()
-                                            .iterator();
-
-                            boolean notFound = true;
-                            while (it.hasNext() && notFound) {
-                                for (PriorityConstraint pc : it.next()) {
-                                    if (pc.hasFreeBoostThreads()) {
-                                        notFound = false;
-                                        this.notify();
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-                try {
-                    requestQueue.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-    }
-
-    /**
      * Method that retrieves the compatible requests from the queue based on a
      * custom policy.
      * 
@@ -386,13 +304,13 @@ public class RequestExecutor implements FutureWaiter, ServingController {
             while (body.isActive()) {
 
                 // get compatible ones from the queue
-                List<Request> rc;
-                rc = policy.runPolicy(compatibility);
+                List<Request> rc = policy.runPolicy(compatibility);
 
                 if (rc.size() >= 0) {
                     for (int i = 0; i < rc.size(); i++) {
                         compatibility.addRunning(rc.get(i));
                     }
+                    
                     synchronized (this) {
                         // add them to the ready set
                         for (int i = 0; i < rc.size(); i++) {
@@ -436,58 +354,7 @@ public class RequestExecutor implements FutureWaiter, ServingController {
         }
     }
 
-    /**
-     * Default scheduling policy. <br>
-     * It will take a request from the queue if it is compatible with all
-     * executing ones and also with everyone before it in the queue. If a
-     * request can not be taken out from the queue, the requests it is invalid
-     * with are marked accordingly so that they are not retried until this one
-     * is finally served.
-     * 
-     * @return
-     */
-    private List<Request> runDefaultPolicy() {
-
-        List<Request> reqs = requestQueue.getInternalQueue();
-        List<Request> ret = new ArrayList<Request>();
-
-        int i, lastIndex;
-        for (i = 0; i < reqs.size(); i++) {
-            lastIndex = -2;
-            if (!invalid.contains(reqs.get(i))
-                    && compatibility.isCompatibleWithExecuting(reqs.get(i))
-                    && (lastIndex =
-                            compatibility.getIndexOfLastCompatibleWith(
-                                    reqs.get(i), reqs.subList(0, i))) == i - 1) {
-                Request r = reqs.get(i);
-                ret.add(r);
-
-                compatibility.addRunning(r);
-
-                if (invalidates.containsKey(reqs.get(i))) {
-                    for (Request ok : invalidates.get(reqs.get(i))) {
-                        invalid.remove(ok);
-                    }
-                    invalidates.remove(reqs.get(i));
-                }
-
-                reqs.remove(i);
-                i--;
-
-            } else if (lastIndex > -2 && lastIndex < i) {
-                lastIndex++;
-                if (!invalidates.containsKey(reqs.get(lastIndex))) {
-                    invalidates.put(reqs.get(lastIndex), new HashSet<Request>());
-                }
-
-                invalidates.get(reqs.get(lastIndex)).add(reqs.get(i));
-                invalid.add(reqs.get(i));
-            }
-        }
-
-        return ret;
-    }
-
+   
     /**
      * Serving and Thread management.
      */

src/Core/org/objectweb/proactive/multiactivity/policy/DefaultServingPolicy.java
@@ -1,0 +1,110 @@
+/*
+ * ################################################################
+ *
+ * ProActive Parallel Suite(TM): The Java(TM) library for
+ *    Parallel, Distributed, Multi-Core Computing for
+ *    Enterprise Grids & Clouds
+ *
+ * Copyright (C) 1997-2012 INRIA/University of
+ *                 Nice-Sophia Antipolis/ActiveEon
+ * Contact: proactive@ow2.org or contact@activeeon.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; version 3 of
+ * the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ * $$PROACTIVE_INITIAL_DEV$$
+ */
+package org.objectweb.proactive.multiactivity.policy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.objectweb.proactive.core.body.request.Request;
+import org.objectweb.proactive.multiactivity.compatibility.StatefulCompatibilityMap;
+
+/**
+ * Interface for describing the scheduling policy to be used in a multi-active
+ * service.
+ * 
+ * @author The ProActive Team
+ */
+public class DefaultServingPolicy implements ServingPolicy {
+
+    private HashSet<Request> invalid = new HashSet<Request>();
+
+    private HashMap<Request, Set<Request>> invalidates =
+            new HashMap<Request, Set<Request>>();
+
+    /**
+     * Default scheduling policy. <br>
+     * It will take a request from the queue if it is compatible with all
+     * executing ones and also with everyone before it in the queue. If a
+     * request can not be taken out from the queue, the requests it is invalid
+     * with are marked accordingly so that they are not retried until this one
+     * is finally served.
+     * 
+     * @return compatible requests to serve.
+     */
+    public List<Request> runPolicy(StatefulCompatibilityMap compatibility) {
+        List<Request> reqs = compatibility.getQueueContents();
+        List<Request> ret = new ArrayList<Request>();
+
+        int i, lastIndex;
+        for (i = 0; i < reqs.size(); i++) {
+            lastIndex = -2;
+            if (!invalid.contains(reqs.get(i))
+                    && compatibility.isCompatibleWithExecuting(reqs.get(i))
+                    && (lastIndex =
+                            compatibility.getIndexOfLastCompatibleWith(
+                                    reqs.get(i), reqs.subList(0, i))) == i - 1) {
+                Request r = reqs.get(i);
+                ret.add(r);
+
+                if (invalidates.containsKey(reqs.get(i))) {
+                    for (Request ok : invalidates.get(reqs.get(i))) {
+                        invalid.remove(ok);
+                    }
+                    invalidates.remove(reqs.get(i));
+                }
+
+                reqs.remove(i);
+                i--;
+
+            } else if (lastIndex > -2 && lastIndex < i) {
+                lastIndex++;
+                if (!invalidates.containsKey(reqs.get(lastIndex))) {
+                    invalidates.put(reqs.get(lastIndex), new HashSet<Request>());
+                }
+
+                invalidates.get(reqs.get(lastIndex)).add(reqs.get(i));
+                invalid.add(reqs.get(i));
+            }
+        }
+
+        return ret;
+    }
+
+}

src/Core/org/objectweb/proactive/multiactivity/policy/ServingPolicy.java
@@ -34,7 +34,7 @@
  * ################################################################
  * $$PROACTIVE_INITIAL_DEV$$
  */
-package org.objectweb.proactive.multiactivity;
+package org.objectweb.proactive.multiactivity.policy;
 
 import java.util.List;
 
@@ -44,8 +44,8 @@ import org.objectweb.proactive.multiactivity.compatibility.StatefulCompatibility
 
 /**
  * Interface for describing the scheduling policy to be used in a multi-active service.
+ * 
  * @author The ProActive Team
- *
  */
 public interface ServingPolicy {
 

src/Core/org/objectweb/proactive/multiactivity/MultiActiveService.java
@@ -47,6 +47,8 @@ import org.objectweb.proactive.core.util.log.ProActiveLogger;
 import org.objectweb.proactive.multiactivity.compatibility.AnnotationProcessor;
 import org.objectweb.proactive.multiactivity.compatibility.CompatibilityTracker;
 import org.objectweb.proactive.multiactivity.execution.RequestExecutor;
+import org.objectweb.proactive.multiactivity.policy.DefaultServingPolicy;
+import org.objectweb.proactive.multiactivity.policy.ServingPolicy;
 import org.objectweb.proactive.multiactivity.priority.PriorityConstraint;
 import org.objectweb.proactive.multiactivity.priority.PriorityManager;
 
@@ -124,7 +126,7 @@ public class MultiActiveService extends Service {
     public void multiActiveServing(int maxActiveThreads, boolean hardLimit, boolean hostReentrant) {
         init();
         executor.configure(maxActiveThreads, hardLimit, hostReentrant);
-        executor.execute();
+        executor.execute(new DefaultServingPolicy());
     }
     
     /**
@@ -137,7 +139,7 @@ public class MultiActiveService extends Service {
     public void multiActiveServing(List<PriorityConstraint> priorityConstraints, int maxActiveThreads, boolean hardLimit, boolean hostReentrant) {
         init(priorityConstraints);
         executor.configure(maxActiveThreads, hardLimit, hostReentrant);
-        executor.execute();
+        executor.execute(new DefaultServingPolicy());
     }
 
     /**
@@ -147,7 +149,7 @@ public class MultiActiveService extends Service {
     public void multiActiveServing(int maxActiveThreads) {
         init();
         executor.configure(maxActiveThreads, false, false);
-        executor.execute();
+        executor.execute(new DefaultServingPolicy());
 
     }
 
@@ -157,7 +159,7 @@ public class MultiActiveService extends Service {
     public void multiActiveServing() {
         init();
         executor.configure(Integer.MAX_VALUE, false, false);
-        executor.execute();
+        executor.execute(new DefaultServingPolicy());
     }
 
     /**
@@ -197,7 +199,7 @@ public class MultiActiveService extends Service {
 
     /**
      * Returns the object through which the service's properties can be modified at run-time.
-     * @return
+     * @return serving controller
      */
     public ServingController getServingController() {
         //this init runs only once even if invoked many times

src/Core/org/objectweb/proactive/multiactivity/ServingController.java
@@ -37,15 +37,15 @@
 package org.objectweb.proactive.multiactivity;
 
 /**
- * Interface for a controller object that can set the properties of geenric multi-active services.
+ * Interface for a controller object that can set the properties of generic multi-active services.
+ * 
  * @author The ProActive Team
- *
  */
 public interface ServingController {
 
     /**
      * Get the number of allowed concurrent servings inside the service.
-     * @return
+     * @return numberOfConcurrent
      */
     public int getNumberOfConcurrent();
 

src/Core/org/objectweb/proactive/multiactivity/policy/ServingPolicyFactory.java
@@ -34,7 +34,7 @@
  * ################################################################
  * $$PROACTIVE_INITIAL_DEV$$
  */
-package org.objectweb.proactive.multiactivity;
+package org.objectweb.proactive.multiactivity.policy;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -47,7 +47,6 @@ import org.objectweb.proactive.multiactivity.compatibility.StatefulCompatibility
  * A factory with examples of serving policies.
  * 
  * @author The ProActive Team
- * 
  */
 @Deprecated
 public class ServingPolicyFactory {

src/Tests/functionalTests/multiactivities/poller/InfiniteCounter.java
@@ -44,7 +44,7 @@ import org.objectweb.proactive.annotation.multiactivity.DefineRules;
 import org.objectweb.proactive.annotation.multiactivity.Group;
 import org.objectweb.proactive.annotation.multiactivity.MemberOf;
 import org.objectweb.proactive.multiactivity.MultiActiveService;
-import org.objectweb.proactive.multiactivity.ServingPolicyFactory;
+import org.objectweb.proactive.multiactivity.policy.ServingPolicyFactory;
 
 
 @DefineGroups( { @Group(name = "gCounter", selfCompatible = false),

src/Tests/functionalTests/multiactivities/scc2/Overhead.java
@@ -50,7 +50,7 @@ import org.objectweb.proactive.api.PAActiveObject;
 import org.objectweb.proactive.core.node.NodeException;
 import org.objectweb.proactive.core.util.wrapper.BooleanWrapper;
 import org.objectweb.proactive.multiactivity.MultiActiveService;
-import org.objectweb.proactive.multiactivity.ServingPolicyFactory;
+import org.objectweb.proactive.multiactivity.policy.ServingPolicyFactory;
 
 
 @DefineGroups(@Group(name = "group", selfCompatible = false))

commit da1855900609ebc065b3f1b3cf06a9321dd4155c

Fixed bug introduced with commit 47fbf8f

src/Core/org/objectweb/proactive/multiactivity/compatibility/StatefulCompatibilityMap.java
@@ -62,6 +62,8 @@ public abstract class StatefulCompatibilityMap extends CompatibilityMap {
         super(clazz);
     }
 
+    public abstract void addRunning(Request request);
+    
     /*
      * Returns the set of methods which are currently executing. Even if 
      * a method is executing in multiple instances, it will appear only 

src/Core/org/objectweb/proactive/multiactivity/execution/RequestExecutor.java
@@ -309,10 +309,6 @@ public class RequestExecutor implements FutureWaiter, ServingController {
                 List<Request> rc = policy.runPolicy(compatibility);
 
                 if (rc.size() >= 0) {
-                    for (int i = 0; i < rc.size(); i++) {
-                        compatibility.addRunning(rc.get(i));
-                    }
-
                     synchronized (this) {
                         // add them to the ready set
                         for (int i = 0; i < rc.size(); i++) {

src/Core/org/objectweb/proactive/multiactivity/policy/DefaultServingPolicy.java
@@ -40,6 +40,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.objectweb.proactive.core.body.request.Request;
@@ -54,9 +55,9 @@ import org.objectweb.proactive.multiactivity.compatibility.StatefulCompatibility
  */
 public class DefaultServingPolicy implements ServingPolicy {
 
-    private HashSet<Request> invalid = new HashSet<Request>();
+    private Set<Request> invalid = new HashSet<Request>();
 
-    private HashMap<Request, Set<Request>> invalidates = new HashMap<Request, Set<Request>>();
+    private Map<Request, Set<Request>> invalidates = new HashMap<Request, Set<Request>>();
 
     /**
      * Default scheduling policy. <br>
@@ -81,6 +82,8 @@ public class DefaultServingPolicy implements ServingPolicy {
                 Request r = reqs.get(i);
                 ret.add(r);
 
+                compatibility.addRunning(r);
+                
                 if (invalidates.containsKey(reqs.get(i))) {
                     for (Request ok : invalidates.get(reqs.get(i))) {
                         invalid.remove(ok);

src/Core/org/objectweb/proactive/multiactivity/policy/ServingPolicy.java
@@ -55,15 +55,17 @@ public interface ServingPolicy {
      * <i>IMPORTANT:</i> While executing a policy the state of the queue and the
      * running set is guaranteed not to change. <br>
      * Please also note this is up to the person that defines the serving policy
-     * to remove the requests that are returned with this method from the
-     * compatibility map passed into parameter. This behavior lets the
-     * possibility to use some caches and thus removing requests from the
-     * compatibility map by index or by value.
+     * to add requests to the running with a call to
+     * {@link StatefulCompatibilityMap#addRunning(Request)} and to remove the
+     * requests that are returned with this method from the compatibility map
+     * passed into parameter through a call to remove on
+     * {@link StatefulCompatibilityMap#getQueueContents()}. This allows to use
+     * some caches and thus to remove requests from the compatibility map by
+     * index or by value.
      * 
-     * @param state
      * @param compatibilityMap
      * 
-     * @return a sublist of the requests that can be started in parallel
+     * @return a sublist of the requests that can be executed in parallel.
      */
     public List<Request> runPolicy(StatefulCompatibilityMap compatibility);
 
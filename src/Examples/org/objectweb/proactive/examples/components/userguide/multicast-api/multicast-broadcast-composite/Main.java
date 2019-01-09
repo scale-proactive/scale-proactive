5fc2fc8fb57530377c8e525898ed23ab1780295c
Françoise Baude Dreysse <francoise.baude@unice.fr>
2019-01-09 12:39:03 +0100

    Added examples of GCM multicast-api

---
+++ src/Examples/org/objectweb/proactive/examples/components/userguide/multicast-api/multicast-broadcast-composite/Main.java
@@ -0,0 +1,155 @@
+/*
+ * ################################################################
+ *
+ * ProActive Parallel Suite(TM): The Java(TM) library for
+ *    Parallel, Distributed, Multi-Core Computing for
+ *    Enterprise Grids & Clouds
+ *
+ * Copyright (C) 1997-2012 INRIA/University of
+ *                 Nice-Sophia Antipolis/ActiveEon
+ * Contact: proactive@ow2.org or contact@activeeon.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; version 3 of
+ * the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ * $$PROACTIVE_INITIAL_DEV$$
+ */
+
+import org.etsi.uri.gcm.api.type.GCMTypeFactory;
+import org.etsi.uri.gcm.util.GCM;
+import org.objectweb.fractal.api.Component;
+import org.objectweb.fractal.api.factory.GenericFactory;
+import org.objectweb.fractal.api.type.ComponentType;
+import org.objectweb.fractal.api.type.InterfaceType;
+import org.objectweb.fractal.api.type.TypeFactory;
+import org.objectweb.proactive.core.component.Constants;
+import org.objectweb.proactive.core.component.ContentDescription;
+import org.objectweb.proactive.core.component.ControllerDescription;
+import org.objectweb.proactive.core.component.Utils;
+import org.objectweb.proactive.core.component.control.PAContentController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author The ProActive Team
+ */
+
+/**
+ * Multicast interface example with broadcast distribution of parameters. The master components multi-casts the request to 3 slaves.
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Component boot = Utils.getBootstrapComponent();
+        GCMTypeFactory gcmTypeFactory = GCM.getGCMTypeFactory(boot);
+        GenericFactory genericFactory = GCM.getGenericFactory(boot);
+        final String MULTICAST_INTERFACE_NAME = "multicast_interface";
+        //define component types
+        ComponentType componentTypeComposit = gcmTypeFactory.createFcType(new InterfaceType[]
+                                                {
+                                                        gcmTypeFactory.createFcItfType
+                                                                ("runner",
+                                                                    Runner.class.getName(),
+                                                                    TypeFactory.SERVER,
+                                                                    TypeFactory.MANDATORY,
+                                                                    TypeFactory.SINGLE
+                                                                )
+                                                });
+
+        ComponentType componentTypeMaster = gcmTypeFactory.createFcType(new InterfaceType[]
+                {
+                        gcmTypeFactory.createFcItfType
+                                ("runner",
+                                    Runner.class.getName(),
+                                    TypeFactory.SERVER,
+                                    TypeFactory.MANDATORY,
+                                    TypeFactory.SINGLE
+                                ),
+                        gcmTypeFactory.createGCMItfType
+                                (
+                                    MULTICAST_INTERFACE_NAME,
+                                    MasterMulticastItf.class.getName(),
+                                    TypeFactory.CLIENT,
+                                    TypeFactory.MANDATORY,
+                                    GCMTypeFactory.MULTICAST_CARDINALITY
+                                )
+                });
+
+        // you can define one component type and use it in all slaves bindings with different names. For the sake of clarity we will make 3 interface type for the 3 slaves
+        ComponentType componentTypeSlave1 = gcmTypeFactory.createFcType(new InterfaceType[]{gcmTypeFactory.createFcItfType("i1", Itf1.class
+                .getName(), TypeFactory.SERVER, TypeFactory.MANDATORY, TypeFactory.SINGLE)});
+
+        ComponentType ComponentTypeSlave2 = gcmTypeFactory.createFcType(new InterfaceType[]{gcmTypeFactory.createFcItfType("i2", Itf1.class
+                .getName(), TypeFactory.SERVER, TypeFactory.MANDATORY, TypeFactory.SINGLE)});
+
+        ComponentType ComponentTypeSlave3 = gcmTypeFactory.createFcType(new InterfaceType[]{gcmTypeFactory.createFcItfType("i3", Itf1.class
+                .getName(), TypeFactory.SERVER, TypeFactory.MANDATORY, TypeFactory.SINGLE)});
+
+        Component composite = genericFactory.newFcInstance(componentTypeComposit, new ControllerDescription("composite",
+                Constants.COMPOSITE), null);
+
+        Component master = genericFactory.newFcInstance(componentTypeMaster,
+                new ControllerDescription("master", Constants.PRIMITIVE),
+                MasterImpl.class.getName()
+        );
+
+        //ContentDescription is used to initialize the "name" member variable in slave. This is how you initialize the member variables in slave if you want.
+        Component slave1 = genericFactory.newFcInstance(componentTypeSlave1, new ControllerDescription("slave1", Constants.PRIMITIVE),
+                new ContentDescription(SlaveImpl.class.getName(), new Object[]{"Slave-1"}));
+        Component slave2 = genericFactory.newFcInstance(ComponentTypeSlave2, new ControllerDescription("slave2", Constants.PRIMITIVE),
+                new ContentDescription(SlaveImpl.class.getName(), new String[]{"Slave-2"}));
+        Component slave3 = genericFactory.newFcInstance(ComponentTypeSlave3, new ControllerDescription("slave3", Constants.PRIMITIVE),
+                new ContentDescription(SlaveImpl.class.getName(), new String[]{"Slave-3"}));
+
+        /**
+         * To add components to the composite component, we need to get the reference of ContentController object of "composite" component.
+         */
+        List<Component> componentList = new ArrayList<>();
+        componentList.add(slave1);
+        componentList.add(slave2);
+        componentList.add(slave3);
+        componentList.add(master);
+        ((PAContentController) GCM.getContentController(composite)).addFcSubComponent(componentList);
+
+        //bindings master ---> 3 slaves
+        GCM.getBindingController(master).bindFc(MULTICAST_INTERFACE_NAME, slave1.getFcInterface("i1"));
+        GCM.getBindingController(master).bindFc(MULTICAST_INTERFACE_NAME, slave2.getFcInterface("i2"));
+        GCM.getBindingController(master).bindFc(MULTICAST_INTERFACE_NAME, slave3.getFcInterface("i3"));
+        //bindings composite ---> master
+        GCM.getBindingController(composite).bindFc("runner", master.getFcInterface("runner"));
+        //start the components via composite
+        GCM.getGCMLifeCycleController(composite).startFc();
+
+        Runner runner = (Runner) composite.getFcInterface("runner");
+        List<String> arg = new ArrayList<String>();
+        arg.add("Hello ");
+        arg.add("from");
+        runner.run(arg);
+
+        Thread.sleep(4000); //this line is to avoid race condition when slaves are killed by main thread before they can perform their assigned tasks. Of course there can be better solution to fix this.
+        GCM.getGCMLifeCycleController(composite).stopFc();
+        System.exit(0);
+    }
+}
---
+++ src/Examples/org/objectweb/proactive/examples/components/userguide/multicast-api/readme.md
@@ -0,0 +1,64 @@
+﻿﻿# About
+This folder contains source code example that will help you start-up with coding some simple GCM applications. You can use any IDE to run these example programs, just copy and paste the source files to your project, and start playing with it. You just need some to do following configurations. 
+
+
+
+# Schema maintained in all examples
+
+There is a **composite** component that holds the **master** and three **slave** components. The composite component is bound to master, and master is further bound to three slaves. We maintain this schema in all the examples, unless specified.  
+
+# What is inside this folder.
+This folder contains following examples
+
+1. **Broadcast**  :The master broadcasts it's input to all the connected slaves. 
+2. **Round-robin** :The master breaks it's input list of elements, and distributes it to slaves in round-robin fashion to slaves. 
+3. **Unicast** : The master sends input to only one of the connected slaves.
+4. **Random**: The master sends the items in the list of input to the connected slaves in random manner.
+5. **One-to-One** The master sends **i-th** item in the list to **i-th** connected slave. The number of items in the list should be equal to number of connected slaves. 
+
+
+
+### Configuring your project
+Before you start running example code , you need to add policy file and JVM parameters to configure your environment to your java project.
+
+###Creating policy file
+Using any text editor, create a file with the name "proactive.java.policy" and paste the following into this file. 
+
+```
+grant 
+{
+  permission java.security.AllPermission;
+
+  // Reflect access private Members
+  // Used by:
+  //         -Unicore Process
+  permission java.lang.RuntimePermission "accessDeclaredMembers";
+  permission java.lang.reflect.ReflectPermission "suppressAccessChecks";
+};
+
+```
+
+ Once you create this security file, place it in the root folder of your java project.  Next step is making JVM aware of this policy file using below JVM parameter. 
+
+```
+-Djava.security.policy="proactive.java.policy" 
+```
+Second JVM parameter you need to add is 
+
+```
+-Dfractal.provider=org.objectweb.proactive.core.component.Fractive
+```
+That's all for the configuration. Have fun with GCM.
+
+# JAR files to add
+There are 4 Jar files that you must add to run these program.
+
+1. _Proactive.jar_ from folder _scale-proactive-master/dist/lib_ 
+2.  _Proactive_Annotations_CTree.jar" from folder _scale-proactive-master/dist/lib_ 
+3. _fractal-gcm-api-1.1.1.jar_ from folder _scale-proactive-master/lib_
+4. _fractal-api-2.0.3-SNAPSHOT.jar_ from folder _scale-proactive-master/lib_
+ 
+
+ 
+
+
